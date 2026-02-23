80fdbc96b5863e8358bc1c613d8114fb499470c7
Bene Leitz <leitzb@cip.ifi.lmu.de>
2026-02-23 14:10:04 +0100

    Redid project structure

--- src/main/java/blockteil/Main.java
+++ src/main/java/blockteil/Main.java
@@ -1,9 +1,9 @@
-package com.gobi.blockteil;
+package blockteil;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("GoBI-Blockteil");
+        System.out.println("Hello, World!");
     }
 
     public int add(int a, int b) {
