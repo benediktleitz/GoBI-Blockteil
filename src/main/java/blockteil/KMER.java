a91a8888ec3bc97b2116c934d5799b166a52fa7c
Bene Leitz <leitzb@cip.ifi.lmu.de>
2026-02-24 11:23:15 +0100

    Initialized KMER for bit manipulation logic

--- src/main/java/blockteil/KMER.java
+++ src/main/java/blockteil/KMER.java
@@ -1,39 +1,71 @@
 package blockteil;
 
-import java.util.Arrays;
-import java.lang.StringBuilder;
-
-
 public class KMER {
-    public byte[] sequence;
 
-    public KMER(byte[] sequence) {
-        this.sequence = sequence;
+    private static final long mask;
+
+    static {
+        mask = (1L << Main.KMER_LENGTH) - 1;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        KMER other = (KMER) obj;
-        if (sequence.length != other.sequence.length) return false;
-        for (int i = 0; i < sequence.length; i++) {
-            if (sequence[i] != other.sequence[i]) return false;
+    private static long getBitRepresentation(char c) {
+        switch (c) {
+            case 'A':
+                return 0b00;
+            case 'C':
+                return 0b01;
+            case 'G':
+                return 0b10;
+            case 'T':
+                return 0b11;
+            default:
+                throw new IllegalArgumentException("Invalid character: " + c);
         }
-        return true;
     }
 
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(sequence);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : sequence) {
-            sb.append((char) b);
+    public static long makeKMER(byte[] sequence, int start) {
+        long kmer = 0;
+        for (int i = 0; i < Main.KMER_LENGTH; i++) {
+            kmer <<= 2;
+            kmer |= getBitRepresentation((char) sequence[start + i]);
         }
-        return sb.toString();
+        return kmer & mask;
+    }
+
+    public static long makeKMER(byte[] sequence) {
+        return makeKMER(sequence, 0);
+    }
+
+    public static long makeKMER(String sequence, int start) {
+        return makeKMER(sequence.getBytes(), start);
+    }
+
+    public static long makeKMER(String sequence) {
+        return makeKMER(sequence.getBytes(), 0);
+    }
+
+    public static long shiftKMER(long kmer, char nextChar) {
+        kmer <<= 2;
+        kmer |= getBitRepresentation(nextChar);
+        return kmer & mask;
+    }
+
+    public static long shiftKMER(long kmer, byte nextByte) {
+        return shiftKMER(kmer, (char) nextByte);
+    }
+
+    public static long shiftKMER(long kmer, byte[] sequence, int index) {
+        return shiftKMER(kmer, (char) sequence[index]);
+    }
+
+    public static long shiftKMER(long kmer, String sequence, int index) {
+        return shiftKMER(kmer, sequence.charAt(index));
+    }
+
+    public static long shiftKMER(long kmer, byte[] sequence, int index, int length) {
+        for (int i = 0; i < length; i++) {
+            kmer = shiftKMER(kmer, sequence[index + i]);
+        }        
+        return kmer;
     }
 }
