f50f8feff80f071faec52a36f8ebb4b434e28ce0
Bene Leitz <leitzb@cip.ifi.lmu.de>
2026-03-05 15:00:24 +0100

    Final adaptations for runtime script

--- bash/runtime_analysis.sh
+++ bash/runtime_analysis.sh
@@ -12,12 +12,31 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT_DIR"
 
-JAR="${JAR:-gta_filter.jar}"
-
-FASTA="${FASTA:-data/pig-genome/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa.gz}"
-GTF="${GTF:-data/pig-genome/Sus_scrofa.Sscrofa11.1.115.chr.gtf.gz}"
-GENES="${GENES:-output/plotting_data/quality/rna/gene_list.txt}"
 OUT_BASE="${OUT_BASE:-test_runtime-analysis}"
+
+
+mkdir -p "$OUT_BASE"
+
+FASTA_MNT="/mnt/raidbio2/extdata/praktikum/genprakt/genprakt-ws25/Block/pig-genome/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa.gz"
+GTF_MNT="/mnt/raidbio2/extdata/praktikum/genprakt/genprakt-ws25/Block/pig-genome/Sus_scrofa.Sscrofa11.1.115.chr.gtf.gz"
+GENES_MNT="/mnt/biocluster/praktikum/genprakt/gruppe_e/gene_list.txt"
+FASTA_IDX_MNT="/mnt/raidbio2/extdata/praktikum/genprakt/genprakt-ws25/Block/pig-genome/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa.gz.fai"
+FASTA_IDX_GZ_MNT="/mnt/raidbio2/extdata/praktikum/genprakt/genprakt-ws25/Block/pig-genome/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa.gz.gzi"
+JAR_MNT="/mnt/biocluster/praktikum/genprakt/gruppe_e/gta_filter.jar"
+
+FASTA="$OUT_BASE/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa.gz"
+GTF="$OUT_BASE/Sus_scrofa.Sscrofa11.1.115.chr.gtf.gz"
+GENES="$OUT_BASE/gene_list.txt"
+FASTA_IDX="$OUT_BASE/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa.gz.fai"
+FASTA_IDX_GZ="$OUT_BASE/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa.gz.gzi"
+JAR="$OUT_BASE/gta_filter.jar"
+
+rsync -avL "$GENES_MNT" "$GENES"
+rsync -avL "$FASTA_MNT" "$FASTA"
+rsync -avL "$GTF_MNT" "$GTF"
+rsync -avL "$FASTA_IDX_MNT" "$FASTA_IDX"
+rsync -avL "$FASTA_IDX_GZ_MNT" "$FASTA_IDX_GZ"
+rsync -avL "$JAR_MNT" "$JAR"
 
 FASTQ_BASE="/mnt/raidbio2/extdata/praktikum/genprakt/genprakt-ws25/Block/pig-data-rnaseq"
 TIME_SUMMARY="$OUT_BASE/time_summary.tsv"
@@ -34,7 +53,7 @@ declare -i total_runs=252 # 21 base settings * 3 runs each * 4 samples = 252 tot
 declare -i executed_runs=0
  
 
-echo -e "k\toffset\tthreshold\tpair_mode\tmode\tsample\telapsed\tuser_sec\tsystem_sec\tmax_rss_kb" > "$TIME_SUMMARY"
+echo -e "k\toffset\tthreshold\tpair_mode\tmode\tsample\telapsed\tuser_sec\tsystem_sec\tmax_rss_kb\tpercent_cpu" > "$TIME_SUMMARY"
 
 extract_time_metric() {
   local key="$1"
@@ -46,17 +65,23 @@ write_time_summary() {
   local time_file="$1"
   local mode="$2"
   local sample="$3"
-
+  local k="$4"
+  local offset="$5"
+  local threshold="$6"
+  local pair_mode="$7"
+  
   local elapsed
   local user_sec
   local system_sec
   local max_rss
+  local percent_cpu
   elapsed="$(extract_time_metric "Elapsed (wall clock) time (h:mm:ss or m:ss)" "$time_file")"
   user_sec="$(extract_time_metric "User time (seconds)" "$time_file")"
   system_sec="$(extract_time_metric "System time (seconds)" "$time_file")"
   max_rss="$(extract_time_metric "Maximum resident set size (kbytes)" "$time_file")"
+  percent_cpu="$(extract_time_metric "Percent of CPU this job got" "$time_file")"
 
-  echo -e "${k}\t${offset}\t${threshold}\t${pair_mode}\t${mode}\t${sample}\t${elapsed:-NA}\t${user_sec:-NA}\t${system_sec:-NA}\t${max_rss:-NA}" >> "$TIME_SUMMARY"
+  echo -e "${k}\t${offset}\t${threshold}\t${pair_mode}\t${mode}\t${sample}\t${elapsed:-NA}\t${user_sec:-NA}\t${system_sec:-NA}\t${max_rss:-NA}\t${percent_cpu:-NA}" >> "$TIME_SUMMARY"
 }
 
 run_one() {
@@ -91,12 +116,12 @@ run_one() {
   # DNA mode run
   /usr/bin/time -v -o "$time_file" "${cmd[@]}"
 
-  write_time_summary "$time_file" "dna" "$sample"
+  write_time_summary "$time_file" "dna" "$sample" "$k" "$offset" "$threshold" "$pair_mode"
 }
 
 for sample in "${SAMPLES[@]}"; do
 
-  r1=$(ls "$FASTQ_BASE"/${sample}-*_R1_001.fastq.gz | head -n1)
+  r1=$(printf "%s\n" "$FASTQ_BASE"/${sample}-*_R1_001.fastq.gz | head -n1)
   prefix=$(basename "$r1" _R1_001.fastq.gz)
 
   FW="$r1"
@@ -115,16 +140,14 @@ for sample in "${SAMPLES[@]}"; do
 
 
   for k in "${K_VALUES[@]}"; do
-    offset="$k"
+    o="$k"
     t=105
     for pair_mode in "${PAIR_MODES[@]}"; do
       for r in $(seq 1 "$RUNS_PER_SETTING"); do
         run_idx=$((run_idx + 1))
-        out_dir="$OUT_BASE"
-        mkdir -p "$out_dir"
 
-        echo "[$run_idx/$total_runs] EXECUTE k=$k offset=$offset threshold=$t pair_mode=$pair_mode"
-        run_one "$k" "$offset" "$t" "$pair_mode" "$out_dir"
+        echo "[$run_idx/$total_runs] EXECUTE k=$k offset=$o threshold=$t pair_mode=$pair_mode"
+        run_one "$k" "$o" "$t" "$pair_mode" "$OUT_BASE"
         executed_runs=$((executed_runs + 1))
       done
     done
@@ -132,15 +155,13 @@ for sample in "${SAMPLES[@]}"; do
 
   for t in "${THRESHOLD_VALUES[@]}"; do
     k=15
-    offset="$k"
+    o="$k"
     for pair_mode in "${PAIR_MODES[@]}"; do
       for r in $(seq 1 "$RUNS_PER_SETTING"); do
         run_idx=$((run_idx + 1))
-        out_dir="$OUT_BASE"
-        mkdir -p "$out_dir"
 
-        echo "[$run_idx/$total_runs] EXECUTE k=$k offset=$offset threshold=$t pair_mode=$pair_mode"
-        run_one "$k" "$offset" "$t" "$pair_mode" "$out_dir"
+        echo "[$run_idx/$total_runs] EXECUTE k=$k offset=$o threshold=$t pair_mode=$pair_mode"
+        run_one "$k" "$o" "$t" "$pair_mode" "$OUT_BASE"
         executed_runs=$((executed_runs + 1))
       done
     done
@@ -152,11 +173,9 @@ for sample in "${SAMPLES[@]}"; do
     for pair_mode in "${PAIR_MODES[@]}"; do
       for r in $(seq 1 "$RUNS_PER_SETTING"); do
         run_idx=$((run_idx + 1))
-        out_dir="$OUT_BASE"
-        mkdir -p "$out_dir"
 
         echo "[$run_idx/$total_runs] EXECUTE k=$k offset=$o threshold=$t pair_mode=$pair_mode"
-        run_one "$k" "$o" "$t" "$pair_mode" "$out_dir"
+        run_one "$k" "$o" "$t" "$pair_mode" "$OUT_BASE"
         executed_runs=$((executed_runs + 1))
       done
     done
@@ -165,5 +184,10 @@ for sample in "${SAMPLES[@]}"; do
   rm -f "$FW" "$RW"
 
 done
+
 echo "Run time analysis finished. Results are in: $OUT_BASE"
 echo "Executed runs: $executed_runs"
+
+mkdir -p /mnt/biocluster/praktikum/genprakt/gruppe_e/plotting_data/runtime_analysis/
+
+rsync -avL "$TIME_SUMMARY" /mnt/biocluster/praktikum/genprakt/gruppe_e/plotting_data/runtime_analysis/
